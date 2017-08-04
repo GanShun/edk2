7aeb3af41e80e1786b46203e8f0a6ba62711e300
Gan Shun <ganshun@gmail.com>
2017-08-04 09:35:48 -0700

    Building linux as an efi binary
    Verified that it's in the FFS with UEFITool.
    
    Signed-off-by: Gan Shun <ganshun@gmail.com>

--- run.sh
+++ run.sh
@@ -2,11 +2,15 @@
 
 OPTS="-M q35 -m 2048 -smp 2"
 OPTS="$OPTS -drive if=pflash,format=raw,file=Build/OvmfX64/DEBUG_GCC48/FV/OVMF.fd"
+#OPTS="$OPTS -drive if=pflash,format=raw,file=Build/OvmfX64/DEBUG_GCC48/FV/OVMFNew.fd"
+#OPTS="$OPTS -drive if=pflash,format=raw,file=~/bios/ocp/board2.dxectomy.replaceshellcode.rom"
+#OPTS="$OPTS -drive if=pflash,format=raw,file=~/bios/ocp/smihell.rom"
 OPTS="$OPTS -L /usr/local/google/home/ganshun/qemu/pc-bios"
 #OPTS="$OPTS -device qxl-vga"
 OPTS="$OPTS -display curses"
 OPTS="$OPTS -monitor /dev/pts/27"
 OPTS="$OPTS -serial /dev/tty"
+OPTS="$OPTS -net none"
 OPTS="$OPTS -global isa-debugcon.iobase=0x402 -debugcon file:fedora.ovmf.log"
 OPTS="$OPTS -global PIIX4_PM.disable_s3=0"
 qemu-system-x86_64 $OPTS
